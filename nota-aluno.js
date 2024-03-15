aa484d9428f868d81cf33416a9b7f6de9b755b1d
Mariana Segovia Penha <158625854+maripenha@users.noreply.github.com>
2024-03-15 18:56:27 +0000

    desafiocorreçao

--- nota-aluno.js
+++ nota-aluno.js
@@ -1,16 +1,29 @@
+//Nome do aluno
+const nomeAluno = "Mariana";
+
 // Definindo as notas do aluno
 const nota1 = 7;
 const nota2 = 6;
 const nota3 = 8;
 
 //Calculando a média
-const media = (nota1 + nota2 + nota3) / 3;
+const mediaFinal = (nota1 + nota2 + nota3) / 3;
 
 //Resultados
-if (media >= 7) {
-    console.log("Aluno aprovado.");
-} else if (media <= 4) {
-    console.log("Aluno reprovado.")
+if (mediaFinal >= 7) {
+    console.log(
+      `A aluna ${nomeAluno} foi aprovada com a média final de ${mediaFinal}.`
+      );
+} else if (mediaFinal <= 4) {
+    console.log(
+      `A aluna ${nomeAluno} foi reprovada com a média final de ${mediaFinal}.`
+      );
+    } else if (mediaFinal <= 6) {
+        console.log(
+          `A aluna ${nomeAluno} ficou de recuperação com a média final de ${mediaFinal}.`
+          );
   } else {
-    ("Aluno em recuperação.")
+    (
+      `Erro!`
+    );
   }
